de92f317dd92928bb09ca9e3415f805bd4e9cfad
Ruan Pablo <ruan.pablogg129@gmail.com>
2025-09-27 15:00:41 -0300

    Adivinhe um número de 0 a 50 fornecido pelo computador

--- projeto1.py
+++ projeto1.py
@@ -1,10 +1,34 @@
+from random import randint
 
-def mult(a, b):
-    s = a * b
-    print(f'A multiplicação entre {a} + {b} = {s}')
+computador = randint(0, 50)
 
+chance = int(input('Quantas tentativas?'))
+tentativas = 0
 
-a = int(input('Digite o Primeiro valor: '))
-b = int(input('Digite o Segundo valor: '))
-mult(a, b)
+while chance != tentativas:
+    print(computador)
+    usuario = int(input('Digite o valor: '))
+    tentativas += 1
+
+    if usuario < computador: 
+        print('É maior...')
+        print('-=' * 20)
+
+    elif usuario > computador:
+        print('É menor...')
+        print('-=' * 20)
+    
+    elif usuario == computador:
+        print('Você acertou o número')
+        print(f'O número sorteado foi o {computador}')
+        break
+
+    if tentativas > chance:
+        break
+
+if usuario != computador:
+    print('Suas tentativas acabaram. Tente novamente!')
+    print(f'O número sorteado foi o {computador}')
+
+print('-=' * 30)
 
